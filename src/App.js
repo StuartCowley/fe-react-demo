b0e956ed70598f6925de01760fd7d24cefa88882
StuartCowley <voidstalker@live.co.uk>
2021-09-28 18:02:22 +0100

    Create posts.json, import into App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import "./App.css";
 import Post from "./components/Post";
 
-function App() {
-  const post = {
-    title: "Learning React",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tempora distinctio sint numquam non, exercitationem veniam quis optio similique odit!",
-    tags: ["react", "manchester", "2021"],
-    author: "May21",
-    date: "27-09-2021",
-    isPublished: true,
-  };
+import posts from './data/posts.json';
 
+function App() {
   return (
     <div className="App">
-      <Post postData={post} />
+      <Post postData={posts[0]} />
     </div>
   );
 }
